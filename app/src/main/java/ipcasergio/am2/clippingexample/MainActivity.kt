ebf925e7ac1029e631ff8a26b570d779a1d1be44
Sérgio <a18074@alunos.ipca.pt>
2020-12-14 18:57:39 +0000

    Initial commit  Complete! -+

---
+++ app/src/main/java/ipcasergio/am2/clippingexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package ipcasergio.am2.clippingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView( this))
+
+
+
+
+    }
+
+
+}
